bdcff30cdbd906132fc716ccc3341c8cae7909dc
KakEnot <k.a.isaeva@icloud.com>
2022-09-03 22:46:59 +0400

    change class Win

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,38 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
+        return checkHorizontal(board) || checkVertical(board);
+    }
+
+    public static boolean checkHorizontal(int[][] board) {
+        boolean rsl = false;
+        for (int i = 0; i < board.length; i++) {
+            int k = 0;
+            for (int j = 0; j < board[i].length; j++) {
+                k += board[i][j];
+            }
+            if (k == board[i].length) {
+                rsl = true;
+                break;
+            }
+        }
         return rsl;
     }
+
+    public static boolean checkVertical(int[][] board) {
+        boolean rsl = false;
+        for (int i = 0; i < board.length; i++) {
+            int k = 0;
+            for (int j = 0; j < board.length; j++) {
+                k += board[j][i];
+            }
+
+            if (k == board.length) {
+                rsl = true;
+                break;
+            }
+        }
+        return rsl;
+    }
+
 }
